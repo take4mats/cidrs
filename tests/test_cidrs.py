5d8d654395b47c9ad2c9df50609d5fb4aa43a988
take4mats <georgeorge.m@gmail.com>
2024-03-11 09:58:23 +0900

    Updated test name

--- tests/test_cidrs.py
+++ tests/test_cidrs.py
@@ -22,16 +22,16 @@ def test_invalid_input():
     assert cidrs.get_errors() == ["'192.168.0' does not appear to be an IPv4 or IPv6 network"]
 
 # IPv4 を集約できる
-def test_summarization_ipv4():
+def test_summarize_ipv4():
     cidrs = CIDRs(['192.168.0.0', '192.168.0.1', '192.168.0.2'])
     assert cidrs.summarize_cidrs() == [ipaddress.IPv4Network('192.168.0.0/31'), ipaddress.IPv4Network('192.168.0.2/32')]
 
 # IPv6 を集約できる
-def test_summarization_ipv6():
+def test_summarize_ipv6():
     cidrs = CIDRs(['2001:db8::', '2001:db8::1', '2001:db8::2'])
     assert cidrs.summarize_cidrs() == [ipaddress.IPv6Network('2001:db8::/127'), ipaddress.IPv6Network('2001:db8::2/128')]
 
 # IPv4 と IPv6 を混在させて集約できる
-def test_summarization_mixed():
+def test_summarize_mixed():
     cidrs = CIDRs(['2001:db8::', '192.168.0.0', '2001:db8::2', '192.168.0.2', '2001:db8::1', '192.168.0.1'])
     assert cidrs.summarize_cidrs() == [ipaddress.IPv4Network('192.168.0.0/31'), ipaddress.IPv4Network('192.168.0.2/32'), ipaddress.IPv6Network('2001:db8::/127'), ipaddress.IPv6Network('2001:db8::2/128')]
